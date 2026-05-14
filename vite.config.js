298b759e93d1734695d167f7d1e5ee93f402f356
DarshitJain25 <darshitjain25oct@gmail.com>
2026-05-14 19:37:50 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ["@vercel/analytics"],
+  },
+  ssr: {
+    // This prevents Vite from trying to transform the peer deps incorrectly
+    noExternal: ["@vercel/analytics", "next"],
+  },
+});
